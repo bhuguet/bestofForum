ebe3d857a0e325ef2701b5a8b4e2061829c4c545
Bernard <bhuguet@bestofmedia.com>
2011-11-16 22:45:35 +0100

    added behatSteps.php and pom.xml

---
+++ test/bdd/steps/behatSteps.php
@@ -0,0 +1,9 @@
+<?php
+
+$steps->Given('/^I browse the "([^"]*)" page$/', function($world, $arg1) {
+    throw new \Behat\Behat\Exception\Pending();
+});
+
+$steps->Then('/^I can find "([^"]*)"$/', function($world, $arg1) {
+    throw new \Behat\Behat\Exception\Pending();
+});
